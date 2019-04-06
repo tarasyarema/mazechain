964d50d78dfcd584c168263ef6df2c54a5f47193
Felix Arribas Pardo <felix.arribas@skyscanner.net>
2019-04-06 23:08:37 +0200

    main connection

--- index.js
+++ index.js
@@ -1,46 +1,85 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 8081;
 
-var template_dir = __dirname + '/templates';
+var template_dir = __dirname;
 
 app.get('/', function(req, res){
     res.sendFile(template_dir + '/index.html');
 });
 
-
-io.on('connection', function(socket) {
-    const user_id = random_id();
-    assigned_ids[user_id] = socket;
-
-    socket.on('disconnect', function(){
-        if (!delete assigned_ids[user_id]) { throw new Error(); }
-        console.log('user \'' + user_id + '\' disconnected -- remaining ' + Object.keys(assigned_ids).length);
-    });
-
-    const num_ids = Object.keys(assigned_ids).length;
-    console.log('user \'' + user_id + '\' connected -- remaining ' + num_ids);
-    if (num_ids === N_dimensions){
-        for (const user_id in assigned_ids) {
-            let sock = assigned_ids[user_id];
-            sock.emit('game', 'ayy lmao');
-        }
-    }
-});
-
-
-var assigned_ids = {};
-function random_id() {
-    new_id = 0;
+function randomId(checkList) {
+    let newId = 0;
     do {
-        new_id = 1+Math.floor(Math.random()*10000000);
-    } while (new_id in assigned_ids);
-    return new_id;
+        newId = Date.now();
+    } while (newId in checkList);
+    return newId;
 }
 
-var N_dimensions = 3;
+let users = {};
+let games = {};
 
+io.on('connection', function(socket) {
+    console.log(socket.id);
+    const userId = randomId(users);
+    let gameId = 0;
+    users[userId] = {
+        _id: userId,
+        socket: socket,
+    };
+    io.emit('setId', userId);
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+    socket.on('disconnect', function() {
+        console.log('disconnect');
+        let index = delete userId[userId];
+        if (index > -1) {
+            users.splice(index, 1);
+        }
+        if (gameId) {
+            index = games[gameId].indexOf(gameId);
+            if (index > -1) {
+                games.splice(index, 1);
+            }
+        }
+    });
+
+    socket.on('setName', function(name) {
+        console.log(`setName: ${name}`);
+        users[userId].name = name;
+    });
+
+    socket.on('createGame', function() {
+        console.log('createGame');
+        gameId = randomId(games);
+        games[gameId] = [ userId ];
+        users[userId].gameId = gameId;
+    });
+
+    socket.on('joinGame', function(id) {
+        console.log(`joinGame: ${id}`);
+        if (id in games) {
+            games[id].append(userId);
+            users[userId].gameId = gameId;
+        } else {
+            return false;
+        }
+        return games[id];
+    });
+
+    socket.on('exitGame', function() {
+        console.log('exitGame');
+        if (gameId in games) {
+            // remove user from game
+            const index = games.indexOf(userId);
+            if (index > -1) {
+                games.splice(index, 1);
+            }
+        }
+        return false;
+    });
+});
+
+http.listen(port, function(){
+    console.log(`listening on *:${port}`);
 });
