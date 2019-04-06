2517d195d693a6e24763c8de71d17cd799f66184
Alaa Moucharrafie <alaa.moucharrafie@gmail.com>
2019-04-06 23:37:55 +0200

    changed port to 3000

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 8081;
+var port = process.env.PORT || 3000;
 
 var template_dir = __dirname;
 
